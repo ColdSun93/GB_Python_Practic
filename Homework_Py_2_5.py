d119d2bc93fd2844b29f3d61e92536a51516802f
aleksandr_sa <coldsun93@mail.ru>
2022-12-17 22:51:16 +0300

    change files Homework 2_5

--- Homework_Py_2_5.py
+++ Homework_Py_2_5.py
@@ -1,1 +1,21 @@
 # Реализуйте алгоритм перемешивания списка.
+
+import random
+size = int(input('введите количество элементов '))
+array = []
+for i in range(size):
+    array.append(random.randint(-10, 10))
+print(array)
+
+# random.shuffle(array)
+
+# Перемешивает левую половину с правой в рандомном порядке
+size_range=int(size/2)
+array2=random.sample(range(size_range, size), size_range)
+for i in range(size_range):
+    temp = array[i]
+    array[i] = array[array2[i]]
+    array[array2[i]] = temp
+
+print(array)
+print(array2)
