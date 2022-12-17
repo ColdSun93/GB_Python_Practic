bb8008e8f4532df50fe931fc17f518c334998ee2
aleksandr_sa <coldsun93@mail.ru>
2022-12-17 22:13:27 +0300

    change files Homework 2_4

--- Homework_Py_2_4.py
+++ Homework_Py_2_4.py
@@ -1,2 +1,20 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+number = int(input('введите число '))
+array = [x for x in range(-number, number+1)]
+print(f'Созданный массив: {array}')
+
+path = 'file.txt'
+ar_elem=[]
+file = open(path, 'r')
+for line in file:
+    ar_elem.append(int(line.replace("\n", "")))
+file.close
+
+p=1
+print(f'Произведение элементов ', end ="")
+for i in ar_elem:
+    p *=array[i]
+    print(f'{array[i]}, ', end ="")
+print(f'= {p}')
