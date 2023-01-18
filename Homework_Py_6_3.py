0ca831f21c44ab79151822bfc844f90b9cf1066d
aleksandr_sa <coldsun93@mail.ru>
2023-01-18 22:29:48 +0300

    change files Homework_Py_6_3.py

--- Homework_Py_6_3.py
+++ Homework_Py_6_3.py
@@ -1,3 +1,19 @@
 # Задача 32:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
+
+def deg_num(num, deg):
+    if deg==0:
+        return 1
+    elif deg==1:
+        return num
+    elif deg<0:
+        return 1/(num*deg_num(num, -deg-1))
+    else:
+        return num*deg_num(num, deg-1)
+
+
+numer = int(input("введите число "))
+degre = int(input("введите степень (целое)  "))
+result = deg_num(numer,degre)
+print(result)
